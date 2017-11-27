144d1abc18d8912dbb0d7ac29d7055e0de9ae7f0
Michael Coughlin <michael.coughlin@colorado.edu>
2017-11-27 06:40:55 +0000

    fixed build scripts

--- passwordManager/build_arm.sh
+++ passwordManager/build_arm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../../sdk.py --function_definitions test_function_definitions.json \
+../../sdk.py --function_definitions function_definitions.json \
 --system_configuration ../../default_system_config.json \
 --program_name password_manager --compilation_config compilation_config.json \
 --build_arm
